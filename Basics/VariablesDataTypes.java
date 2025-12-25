dcb250e8879fe24fda4b5a2cff1d943a570558f0
Preethika Amulya Sahithi Chokkakula <vtu30617@veltech.edu.in>
2025-12-25 21:43:18 +0530

    Remove unnecessary blank lines in VariablesDataTypes

---
+++ Basics/VariablesDataTypes.java
@@ -0,0 +1,14 @@
+public class VariablesDataTypes {
+    public static void main(String[] args) {
+        int age = 18;
+        float marks = 85.5f;
+        double price = 999.99;
+        char grade = 'A';
+        boolean isPassed = true;
+        System.out.println("Age = " + age);
+        System.out.println("Marks = " + marks);
+        System.out.println("Price = " + price);
+        System.out.println("Grade = " + grade);
+        System.out.println("Passed? " + isPassed);
+    }
+}
