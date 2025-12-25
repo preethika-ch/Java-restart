de745dea08b89357942333c67472a66ba5debe82
Preethika Amulya Sahithi Chokkakula <vtu30617@veltech.edu.in>
2025-12-25 21:47:24 +0530

    learning data types

--- Basics/VariablesDataTypes.java
+++ Basics/VariablesDataTypes.java
@@ -1,14 +1,16 @@
 public class VariablesDataTypes {
     public static void main(String[] args) {
+        String name = "amulya";
         int age = 18;
         float marks = 85.5f;
         double price = 999.99;
         char grade = 'A';
         boolean isPassed = true;
+        System.out.println("Name = " + name);
         System.out.println("Age = " + age);
         System.out.println("Marks = " + marks);
         System.out.println("Price = " + price);
         System.out.println("Grade = " + grade);
-        System.out.println("Passed? " + isPassed);
+        System.out.println("Passed " + isPassed);
     }
 }
