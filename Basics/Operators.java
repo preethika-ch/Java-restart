0f2575008821b671cc5e3e72bbb04d40ac272e2a
Preethika Amulya Sahithi Chokkakula <vtu30617@veltech.edu.in>
2025-12-25 21:50:05 +0530

    learning operators

---
+++ Basics/Operators.java
@@ -0,0 +1,12 @@
+public class OperatorsDemo {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Remainder = " + (a % b));
+        System.out.println("Is a greater? " + (a > b));
+        System.out.println("Is a equal b? " + (a == b));
+    }
+}
